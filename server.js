7c55eca192d0519cb368a0a2a1bc9c2a04fa0666
subrat7776 <subratbadasethsubrat@gmail.com>
2026-02-23 23:00:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Video Server Running');
+  res.json({ message: "Backend Connected Successfully 🚀" });
 });
 
 app.listen(port, () => {
